83dbb28c8c0ffcc054a7e28364d145fbd4c6d5f9
Josh McArthur <joshua.mcarthur@gmail.com>
2011-08-29 09:11:19 +1200

    Add Platform class to store things like CPU, RAM total, ec

--- lib/monittr_decorator.rb
+++ lib/monittr_decorator.rb
@@ -1,3 +1,17 @@
+class Monittr::Platform
+  attr_accessor :kernel
+  attr_accessor :architecture
+  attr_accessor :num_cpus
+  attr_accessor :total_memory
+#
+  def initialize(xml)
+    self.kernel = xml.xpath("//release").first.content.to_s rescue nil
+    self.architecture = (xml.xpath("//machine").first.content.to_s rescue "") =~ /_64/ ? "64-bit" : "32-bit"
+    self.num_cpus = xml.xpath("//cpu").first.content.to_i rescue nil
+    self.total_memory = xml.xpath("//memory").first.content.to_i rescue nil
+  end
+end
+
 Monittr::Server.class_eval do
 
   class Error
@@ -14,12 +28,12 @@ Monittr::Server.class_eval do
 
   attr_accessor :error
   attr_accessor :id
+  attr_reader :platform
 
   def initialize(url, xml)
     @url = url
     @xml = Nokogiri::XML(xml)
-    @id = @xml.xpath("/monit/server/id").first
-    @id = @id.content if @id
+    @id = @xml.xpath("/monit/server/id").first.content rescue nil
     if error = @xml.xpath('error').first
       @error = Error.new({
         :name => error.attributes['name'].content,
@@ -27,12 +41,14 @@ Monittr::Server.class_eval do
         :status => 3
       })
 
+      @platform = nil
       @system = nil
       @filesystems = []
       @processes = []
       @hosts = []
     else
       @system = Monittr::Services::System.new(@xml.xpath("//service[@type=5]").first)
+      @platform = Monittr::Platform.new(@xml.xpath("//platform").first)
       @filesystems = @xml.xpath("//service[@type=0]").map do |xml|
         Monittr::Services::Filesystem.new(xml)
       end
