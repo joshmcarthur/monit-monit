26bc1f01083ab4f48cf57cd9e52074cebcb897eb
Josh McArthur <joshua.mcarthur@gmail.com>
2011-08-31 10:04:22 +1200

    Added background process for retrieving monit details. Added middleware to control said process

---
+++ lib/monit_retriever.rb
@@ -0,0 +1,30 @@
+$LOAD_PATH.unshift(Dir.pwd + '/models') unless $LOAD_PATH.include?(Dir.pwd + '/models')
+$LOAD_PATH.unshift(Dir.pwd + '/lib') unless $LOAD_PATH.include?(Dir.pwd + '/lib')
+
+require "monit_job"
+require 'rufus/scheduler'
+require 'datamapper'
+require 'server'
+
+class MonitRetriever
+  attr_accessor :jobs
+  attr_accessor :scheduler
+
+  def initalize
+    self.scheduler = Rufus::Scheduler.start_new
+    self.jobs = []
+  end
+
+  def start
+    #Start a new job for each server? Or just one to handle all of them?
+    Server.each do |server|
+      self.jobs << scheduler.every('1m', MonitJob.new(server.id))
+    end
+    self
+  end
+
+  def stop
+    self.jobs.each { |j| self.scheduler.unschedule(j.job_id) }
+  end
+end
+
