214ee0bd55cf07377674afc90399c1c6da58fcfe
Josh McArthur <joshua.mcarthur@gmail.com>
2011-08-31 10:05:13 +1200

    Cleaned up requires

--- models/resource_record.rb
+++ models/resource_record.rb
@@ -1,15 +1,18 @@
 require 'dm-timestamps'
+require 'server'
+require 'server_process'
 
 class ResourceRecord
   include DataMapper::Resource
-  
+
   property :id, Serial
   property :cpu, Float, :required => true
   property :memory, Float, :required => true
   property :load, Float
   property :swap, Float
   property :created_at, DateTime
-  
+
   belongs_to :server, :required => false
   belongs_to :process, 'ServerProcess', :required => false
 end
+
